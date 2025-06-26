5e51688e34e50d34b8c54affec6777c60706fcb4
Coss992 <marcosgarciacosme@gmail.com>
2025-06-26 18:33:34 +0200

    Agregar archivos de proyecto.

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener("load", () => {
+    const loader = document.getElementById("loaderModal");
+    setTimeout(() => {
+        loader.style.transition = "opacity 0.5s ease";
+        loader.style.opacity = "0";
+        setTimeout(() => loader.remove(), 500);
+    }, 2000);
+});
+
+window.addEventListener("mousedown", () => {
+    document.body.style.cursor = 'url("img/cursorclick.cur"), auto';
+});
+window.addEventListener("mouseup", () => {
+    document.body.style.cursor = ""; // vuelve al cursor definido en CSS
+});
